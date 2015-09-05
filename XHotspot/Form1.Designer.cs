79afc200e1af574614a78bfdc12564e33c30bab0
Sohel Aman Khan <sohelamankhan@gmail.com>
2015-09-05 19:07:05 +0600

    Version 1.0

--- XHotspot/Form1.Designer.cs
+++ XHotspot/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -46,9 +47,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(10, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.Size = new System.Drawing.Size(105, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hotspot";
+            this.label1.Text = "xHotspot";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
@@ -64,16 +65,20 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(75, 75);
+            this.textBox1.MaxLength = 128;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(123, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "xHotspot";
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(75, 102);
+            this.textBox2.MaxLength = 128;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(123, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // label2
             // 
@@ -96,11 +101,12 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(88, 133);
+            this.label4.Location = new System.Drawing.Point(74, 135);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(179, 13);
+            this.label4.Size = new System.Drawing.Size(196, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "* Min length of SSID=4, Password=8";
+            this.label4.Text = "* Min length for SSID is 4, Password is 8";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
@@ -123,17 +129,29 @@
             // 
             this.button2.Location = new System.Drawing.Point(208, 46);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(64, 23);
+            this.button2.Size = new System.Drawing.Size(37, 23);
             this.button2.TabIndex = 10;
             this.button2.Text = "Info";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(249, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label5);
@@ -165,6 +183,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- XHotspot/Form1.cs
+++ XHotspot/Form1.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,10 +15,69 @@ namespace XHotspot
 {
     public partial class Form1 : Form
     {
+        const int START = 1;
+        const int STOP = 2;
+        const int ALLOW = 3;
+        int buttonAction = 0;
+
         public Form1()
         {
             InitializeComponent();
-            textBox3.Text = "Welcome!";
+            string s = ExecuteCommand("netsh wlan show hostednetwork");
+            string allow = "";
+            if (s.Contains("Mode"))
+            {
+                StringReader sr = new StringReader(s);
+                while (sr.Peek() >= 0)
+                {
+                    string tmp = sr.ReadLine();
+                    if (tmp.Contains("Mode"))
+                    {
+                        allow = tmp.Replace(" ", String.Empty);
+                        break;
+                    }
+                }
+            }
+
+            if (!allow.Contains(":Allowed"))
+            {
+                buttonAction = ALLOW;
+                button1.Text = "Allow";
+                textBox3.Text = "Not allowed";
+            }
+            else
+            {
+                textBox3.Text = "Howdy, buddy?";
+                string status = "";
+                if (s.Contains("Status  "))
+                {
+                    StringReader sr2 = new StringReader(s);
+                    while (sr2.Peek() >= 0)
+                    {
+                        string tmp2 = sr2.ReadLine();
+                        if (tmp2.Contains("Status  "))
+                        {
+                            status = tmp2.Replace(" ", String.Empty);
+                            break;
+                        }
+                    }
+                }
+                //textBox3.Text = status;
+                if (status.Contains(":Started"))
+                {
+                    buttonAction = STOP;
+                    button1.Text = "Stop";
+                    textBox3.Text = "Hotspot running";
+                }
+                else
+                {
+                    buttonAction = START;
+                    button1.Text = "Start";
+                    textBox3.Text = "Hotspot not running";
+                }
+            }
+
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -27,30 +87,98 @@ namespace XHotspot
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string s = ExecuteCommand("netsh wlan show hostednetwork");
-            if (s.Contains("Mode"))
+            if (buttonAction == ALLOW)
             {
-                StringReader sr = new StringReader(s);
-                while (sr.Peek() >= 0)
+                button1.Text = "Start";
+                buttonAction = START;
+                MessageBox.Show(ExecuteCommand("netsh wlan set hostednetwork mode=allow"), "Result");
+
+            }
+            else if (buttonAction == START)
+            {
+                string ssid, pass;
+                ssid = textBox1.Text;
+                pass = textBox2.Text;
+                string validation = validateCredintials(ssid, pass);
+                //textBox3.Text = validation;
+                string s = ExecuteCommand("netsh wlan show hostednetwork");
+                string allow = "";
+                if (s.Contains("Mode"))
                 {
-                    string tmp = sr.ReadLine();
-                    if (tmp.Contains("Mode"))
+                    StringReader sr = new StringReader(s);
+                    while (sr.Peek() >= 0)
                     {
-                        textBox3.Text = tmp.Replace(" ", String.Empty);
-                        break;
+                        string tmp = sr.ReadLine();
+                        if (tmp.Contains("Mode"))
+                        {
+                            allow = tmp.Replace(" ", String.Empty);
+                            break;
+                        }
                     }
                 }
 
+                if (allow.Contains(":Allowed"))
+                {
+                    if (validation.Equals("OK"))
+                    {
+
+                        string command = "netsh wlan set hostednetwork mode=allow ssid="+ssid+" key="+pass;
+                        string abc = ExecuteCommand(command);
+                        if(abc.Contains("successfully")) {
+                            string res = ExecuteCommand("netsh wlan start hostednetwork");
+                            if (res.Contains("started"))
+                            {
+                                buttonAction = STOP;
+                                button1.Text = "Stop";
+                                textBox3.Text = "Hotspot started";
+                                MessageBox.Show(res, "Result");
+                            }
+                            else
+                            {
+                                textBox3.Text = "Error starting hotspot";
+                                MessageBox.Show(res, "Result");
+                            }
+                        }
+                        else
+                        {
+                            textBox3.Text = "Couldn't setup hotspot";
+                            MessageBox.Show(abc, "Result");
+                        }
+                        
+                    }
+                    else
+                    {
+                        textBox3.Text = validation;
+                    }
+                }
+                else
+                {
+                    textBox3.Text = "Hotspot not allowed";
+                    button1.Text = "Allow";
+                    buttonAction = ALLOW;
+                }
             }
-            //textBox3.Text = 
+            else if (buttonAction == STOP)
+            {
+                buttonAction = START;
+                button1.Text = "Start";
+                textBox3.Text = "Hotspot not running";
+                MessageBox.Show(ExecuteCommand("netsh wlan stop hostednetwork"), "Result");
+            }
+            else
+            {
+                MessageBox.Show("Something really bad happened!", "OMG");
+            }
+            
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Created by Sohel Aman \nsohelamankhan@gmail.com!", "About");
+            MessageBox.Show("Created by Sohel Aman \nsohelamankhan@gmail.com", "About");
         }
 
-        string ExecuteCommand(string command)
+        private string ExecuteCommand(string command)
         {
             int exitCode;
             ProcessStartInfo processInfo;
@@ -79,14 +207,68 @@ namespace XHotspot
             return outText;
         }
 
+        private string validateCredintials(string id, string pas)
+        {
+            bool ssidOK = false;
+            if (id.Length < 4)
+            {
+                return "SSID too short";
+            }
+            else
+            {
+                char first = id[0];
+                int val=(int)first;
+                if ((val >= 65 && val <= 90) || (val >= 97 && val <= 122))
+                {
+                    ssidOK = true;
+                }
+                else
+                {
+                    return "Invalid SSID";
+                }
+            }
+            if (ssidOK)
+            {
+                if (pas.Length < 8)
+                {
+                    return "Password too short";
+                }
+            }
+            return "OK";
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show(ExecuteCommand("netsh wlan show hostednetwork"), "Information");
         }
 
-        //static void Main()
-        //{
-        //    ExecuteCommand("echo testing");
-        //}
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s1 = ExecuteCommand("netsh wlan show drivers");
+            string s2 = "";
+            if (s1.Contains("Interface name"))
+            {
+                StringReader sr = new StringReader(s1);
+                while (sr.Peek() >= 0)
+                {
+                    string tmp = sr.ReadLine();
+                    string tmp2 = Regex.Replace(tmp, @"\s+", " ");
+                    s2 += tmp2 + "\n\r";
+                    if (tmp.Contains("Hosted network supported"))
+                    {
+                        break;
+                    }
+                }
+
+            }
+            //MessageBox.Show(ExecuteCommand("netsh wlan show drivers"), "WLAN Details");
+            MessageBox.Show(s2, "Details");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
